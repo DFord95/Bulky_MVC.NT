04f9abbd99119ec7de2f2cc3da13ac2ead88c778
Diontae Ford <forddt@dot.state.al.us>
2024-04-01 13:29:47 -0500

    Updated Site UI

---
+++ Bulky.DataAccess/Migrations/20240325184205_addCompanyRecords.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BulkyBookBook.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addCompanyRecords : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "Id", "City", "Name", "PhoneNumber", "PostalCode", "State", "StreetAddress" },
+                values: new object[,]
+                {
+                    { 1, "Tech City", "Tech-Solutions", "8881238324", "60657", "IL", "123 Tech St." },
+                    { 2, "Casual City", "DFord Casualties", "8882227825", "23407", "VA", "456 Casual Ln" },
+                    { 3, "Black Beach", "Back'd By Black", "8884625225", "32654", "FL", "789 Black Blvd" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
